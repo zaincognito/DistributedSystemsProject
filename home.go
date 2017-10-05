570cc390ee79c63672c0e2cb3baa569dfbd42943
na1489 <na1489@nyu.edu>
2017-10-04 21:27:02 -0400

    fixed logout

--- home.go
+++ home.go
@@ -6,7 +6,6 @@ import (
     "strings"
     "time"
     "fmt"
-    "log"
 )
 
 var users = make(map[string][]string)
@@ -31,25 +30,21 @@ func handlerSignin(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerLogout(w http.ResponseWriter, r *http.Request) {
+	c, _ := r.Cookie("username")
+	c.Value = ""
+	c.Path = "/"
+	c.MaxAge = -1
+	http.SetCookie(w,c)
+	http.Redirect(w,r,"/signin",http.StatusPermanentRedirect)
+}
+
 func handlerProfile(w http.ResponseWriter, r *http.Request) {
     temp, _ := template.ParseFiles("profile.html")
     temp.Execute(w,"home.go")
     userN, _ := r.Cookie("username")
     nameP := users[userN.Value][1]
 	fmt.Fprintf(w,"Welcome ", nameP)
-	if r.Method == "POST" {
-		r.ParseForm()
-		log.Printf("Hello1")
-		if(strings.Join(r.Form["logout"], " ") == "Logout") {
-			log.Printf("Hello2")
-			c, _ := r.Cookie("username")
-			c.Value = ""
-			c.Path = "/"
-			c.MaxAge = -1
-			http.SetCookie(w,c)
-			http.Redirect(w,r,"/login",http.StatusPermanentRedirect)
-		}
-	}
 }
 
 func handlerSignup(w http.ResponseWriter, r *http.Request) {
@@ -73,5 +68,6 @@ func main() {
     http.HandleFunc("/signin", handlerSignin)
     http.HandleFunc("/signup", handlerSignup)
     http.HandleFunc("/profile", handlerProfile)
-    http.ListenAndServe(":8080", nil)
+    http.HandleFunc("/logout", handlerLogout)
+    http.ListenAndServe(":8066", nil)
 }
